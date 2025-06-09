ce7f888680b46ca7bf6ffb516245a60df25b5a0e
Stevens <stevenchauvets@gmail.com>
2025-06-09 13:28:55 +0200

    Jeu du pendu final

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 function searchGuess(arr, val){
     return arr.indexOf(val)
 }
@@ -8,33 +9,29 @@ function guess(partyGuess){
     partyGuess = Number(prompt("Combien de tour voulez vous faire ?"))
     for(let i = 0; i < partyGuess; i = i + 1){
 
-    let countGuess = String(prompt("Ajouter " + partyGuess + " mots du pendu !"))
+        let countGuess = String(prompt("Ajouter " + partyGuess + " mots du pendu !"))
         tab.push(countGuess) }
 
     let robots = Math.floor(Math.random() * partyGuess)
-
-    let tabs = [];
-    let tablo = [];
+    let tabs = []
+    let tablo = []
     let stockGuess = Array.from(tab[robots]);
 
     for(let j = 0; j < stockGuess.length; j = j + 1){
         enunciateGuess = prompt("Taper les lettres pour deviner le mot du pendu !")
-        tabs = Array(stockGuess.length).fill("") // ['', '', '', '', '', '', '', '']
-        // []['e', 'e', 'e'][]['e'][][]
-        // Il est preferable d'utiliser la methode fill plutot que d'utiliser
-        // tabs.push('') vos mieux priorisé des données comme sur le code ci-dessus
+        tabs.push("") // ['', '', '', '']
         if (searchGuess(stockGuess, enunciateGuess) !== -1) {
             tabs.splice(j, 1, enunciateGuess)
         } else if(searchGuess(stockGuess, enunciateGuess) > -1){
             console.log(stockGuess + " existe déjà dans le tableau.");
-        } 
+        }
             let index = tabs.filter((word) => word === stockGuess[j]);
             tablo.push(index)
-        }
+    } 
+    console.log(tablo)
 
-        console.log(tablo)
         let reponseGuess = Array.from(stockGuess); 
-        console.log(reponseGuess); // ['v', 'o', 'i', 't', 'u', 'r', 'e'] 
+        console.log(reponseGuess); // ['m', 'o', 'm', 'o'] 
 }
 
  guess()
