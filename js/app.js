5cc61de73439c23a368d4b9087061c5761b68cbc
Stevens <stevenchauvets@gmail.com>
2025-06-09 18:45:36 +0200

    Modification de la recherche de valeur dans le tableau du pendu !

--- js/app.js
+++ js/app.js
@@ -1,37 +1,36 @@
 
-function searchGuess(arr, val){
-    return arr.indexOf(val)
-}
+function searchGuess(arr, val){ return arr.indexOf(val) }
 
 function guess(partyGuess){
 
     let tab = [];
     partyGuess = Number(prompt("Combien de tour voulez vous faire ?"))
     for(let i = 0; i < partyGuess; i = i + 1){
+    let countGuess = String(prompt("Ajouter " + partyGuess + " mots du pendu !"))
+    tab.push(countGuess) }
 
-        let countGuess = String(prompt("Ajouter " + partyGuess + " mots du pendu !"))
-        tab.push(countGuess) }
+    let tabs = [], robots = Math.floor(Math.random() * partyGuess),
+        stockGuess = Array.from(tab[robots]); stockGuess;
+        console.log(stockGuess)
 
-    let robots = Math.floor(Math.random() * partyGuess)
-    let tabs = []
-    let tablo = []
-    let stockGuess = Array.from(tab[robots]);
+        tabs = Array(stockGuess.length).fill("")
 
-    for(let j = 0; j < stockGuess.length; j = j + 1){
-        enunciateGuess = prompt("Taper les lettres pour deviner le mot du pendu !")
-        tabs.push("") // ['', '', '', '']
-        if (searchGuess(stockGuess, enunciateGuess) !== -1) {
-            tabs.splice(j, 1, enunciateGuess)
-        } else if(searchGuess(stockGuess, enunciateGuess) > -1){
-            console.log(stockGuess + " existe déjà dans le tableau.");
+        for(let i = 0; i < tabs.length; i = i + 1){
+        let enunciateGuess = prompt("Taper les lettres pour deviner le mot du pendu !")
+        if(searchGuess(stockGuess, enunciateGuess) !== -1){
+        for(let j = 0; j < stockGuess.length; j = j + 1){ 
+            if(enunciateGuess == stockGuess[j]){
+                tabs.splice(j, 1, enunciateGuess)
+            }
+                console.log("Lettres trouvés : " + stockGuess[j])
+            } 
+            console.log(tabs)
+        } else {
+            console.log("Lettres pas trouvés : " + undefined)
         }
-            let index = tabs.filter((word) => word === stockGuess[j]);
-            tablo.push(index)
-    } 
-    console.log(tablo)
-
-        let reponseGuess = Array.from(stockGuess); 
-        console.log(reponseGuess); 
-}
+        }
+        console.log(tabs)
+        console.log(stockGuess)
+    }
 
  guess()
